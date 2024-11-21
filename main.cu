566b7a21095e413dad8bc56acc826e06b4a8c83a
ppggw <savvin2002@list.ru>
2024-11-22 01:54:46 +0300

    возможность находить несколько максимумов

--- main.cu
+++ main.cu
@@ -14,6 +14,8 @@
 #define THREAD_DIM 32
 #define GRID_SIZE_X 40
 #define GRID_SIZE_Y 22
+#define NUMBER_OF_POINTS 10
+
 
 typedef struct CustomPoints_
 {
@@ -23,12 +25,20 @@ typedef struct CustomPoints_
 } CustomPoint;
 
 
-__device__ CustomPoint array_result[GRID_SIZE_X * GRID_SIZE_Y];
+__device__ CustomPoint cache_for_vert_array[GRID_SIZE_Y];
+__device__ CustomPoint cached_block_result[GRID_SIZE_Y * GRID_SIZE_X];
 
 
-__global__ void GetMaxGrid(unsigned char* image, int rows, int cols){
-    int x = blockIdx.x * blockDim.x + threadIdx.x;
-    int y = blockIdx.y * blockDim.y + threadIdx.y;
+__global__ void GetMaxGrid(unsigned char* image, int rows, int cols, int offset_X, int offset_Y){
+    int x, y;
+    if(offset_Y == 0 && offset_X == 0){
+        x = blockIdx.x * blockDim.x + threadIdx.x;
+        y = blockIdx.y * blockDim.y + threadIdx.y;
+    }
+    else{
+        x = offset_X * blockDim.x + threadIdx.x;
+        y = offset_Y * blockDim.y + threadIdx.y;
+    }
 
     if(x > cols || y > rows){
         return;
@@ -45,8 +55,6 @@ __global__ void GetMaxGrid(unsigned char* image, int rows, int cols){
     __shared__ CustomPoint secondVert[THREAD_DIM/4];
     __shared__ CustomPoint thirdVert[THREAD_DIM/8];
     __shared__ CustomPoint fourthVert[THREAD_DIM/16];
-
-    CustomPoint block_result;
 
     if(threadIdx.x % 2 == 0){
         if(image[y*cols + x] >= image[y*cols + x + 1]){
@@ -139,62 +147,88 @@ __global__ void GetMaxGrid(unsigned char* image, int rows, int cols){
         // для послдней строки не работает. Где-то выход за пределы скорее всего
         if(threadIdx.y % 2 == 0 && threadIdx.y < 2){
             if(fourthVert[threadIdx.y].value  >= fourthVert[ threadIdx.y + 1].value){
-                //result_x[blockIdx.x + blockIdx.y * GRID_SIZE_X] = (int)fourthVert[threadIdx.y].x;
-                //result_y[blockIdx.x + blockIdx.y * GRID_SIZE_X] = (int)fourthVert[threadIdx.y].y;
-                //result_value[blockIdx.x + blockIdx.y * GRID_SIZE_X] = (int)fourthVert[threadIdx.y].value;
-
                 if(fourthVert[threadIdx.y].value <= 255){
-                    array_result[blockIdx.x + blockIdx.y * GRID_SIZE_X] = fourthVert[threadIdx.y];
+                    if(offset_X == 0 && offset_Y == 0){
+                        cached_block_result[blockIdx.x + blockIdx.y * GRID_SIZE_X] = fourthVert[threadIdx.y];
+                    }
+                    else{
+                        cached_block_result[offset_X + offset_Y * GRID_SIZE_X] = fourthVert[threadIdx.y];
+                    }
                     __threadfence();
                 }
                 else{
-                    array_result[blockIdx.x + blockIdx.y * GRID_SIZE_X].x = 0;
-                    array_result[blockIdx.x + blockIdx.y * GRID_SIZE_X].y = 0;
-                    array_result[blockIdx.x + blockIdx.y * GRID_SIZE_X].value = 0;
+                    cached_block_result[blockIdx.x + blockIdx.y * GRID_SIZE_X].x = 0;
+                    cached_block_result[blockIdx.x + blockIdx.y * GRID_SIZE_X].y = 0;
+                    cached_block_result[blockIdx.x + blockIdx.y * GRID_SIZE_X].value = 0;
                 }
             }
             else{
                 if(fourthVert[threadIdx.y + 1].value <= 255){
-                    array_result[blockIdx.x + blockIdx.y * GRID_SIZE_X] = fourthVert[threadIdx.y + 1];
+                    if(offset_X == 0 && offset_Y == 0){
+                        cached_block_result[blockIdx.x + blockIdx.y * GRID_SIZE_X] = fourthVert[threadIdx.y + 1];
+                    }
+                    else{
+                        cached_block_result[offset_X + offset_Y * GRID_SIZE_X] = fourthVert[threadIdx.y + 1];
+                    }
                     __threadfence();
                 }
                 else{
-                    array_result[blockIdx.x + blockIdx.y * GRID_SIZE_X].x = 0;
-                    array_result[blockIdx.x + blockIdx.y * GRID_SIZE_X].y = 0;
-                    array_result[blockIdx.x + blockIdx.y * GRID_SIZE_X].value = 0;
+                    cached_block_result[blockIdx.x + blockIdx.y * GRID_SIZE_X].x = 0;
+                    cached_block_result[blockIdx.x + blockIdx.y * GRID_SIZE_X].y = 0;
+                    cached_block_result[blockIdx.x + blockIdx.y * GRID_SIZE_X].value = 0;
                 }
-            }
-//            printf("val %d x %d y %d\n", array_result[blockIdx.x + blockIdx.y * GRID_SIZE_X].value,
-//                    array_result[blockIdx.x + blockIdx.y * GRID_SIZE_X].x,
-//                    array_result[blockIdx.x + blockIdx.y * GRID_SIZE_X].y);
+            }      
+//            printf("val %d x %d y %d\n", cached_block_result[blockIdx.x + blockIdx.y * GRID_SIZE_X].value,
+//                    cached_block_result[blockIdx.x + blockIdx.y * GRID_SIZE_X].x,
+//                    cached_block_result[blockIdx.x + blockIdx.y * GRID_SIZE_X].y);
         }
     }
 }
 
 
-__global__ void FindVertMax(int* result_x, int* result_y, int* result_value){
-    __shared__ CustomPoint VerticalBlockRes[GRID_SIZE_Y];
-
-    CustomPoint max = array_result[threadIdx.y * GRID_SIZE_X];
+__global__ void FindVertMaxAll(unsigned char* image, CustomPoint* result, int cols){
+    CustomPoint max = cached_block_result[threadIdx.y * GRID_SIZE_X];
     for(int i = 1; i != GRID_SIZE_X; i++){
-        if(array_result[i + threadIdx.y * GRID_SIZE_X].value > max.value){
-            max = array_result[i + threadIdx.y * GRID_SIZE_X];
+        if(cached_block_result[i + threadIdx.y * GRID_SIZE_X].value > max.value){
+            max = cached_block_result[i + threadIdx.y * GRID_SIZE_X];
         }
     }
-    VerticalBlockRes[threadIdx.y] = max;
+    cache_for_vert_array[threadIdx.y] = max;
     __syncthreads();
 
     if(threadIdx.y == 0){
-        CustomPoint maxI = VerticalBlockRes[0];
+        CustomPoint maxI = cache_for_vert_array[0];
+        int coor_max_for_delete = 0;
         for(int i = 1; i != GRID_SIZE_Y; i++){
-            if(VerticalBlockRes[i].value > maxI.value){
-                maxI = VerticalBlockRes[i];
+            if(cache_for_vert_array[i].value > maxI.value){
+                maxI = cache_for_vert_array[i];
+                coor_max_for_delete = i;
             }
         }
-        *result_x = maxI.x;
-        *result_y = maxI.y;
-        *result_value = maxI.value;
+        *result = maxI;
+        image[maxI.y * cols + maxI.x] = 0;
     }
+}
+
+
+__global__ void FindVertMaxOne(unsigned char* image, CustomPoint* result, int coorForReFind, int cols){
+    CustomPoint max = cached_block_result[coorForReFind * GRID_SIZE_X];
+    for(int i = 1; i != GRID_SIZE_X; i++){
+        if(cached_block_result[i + coorForReFind * GRID_SIZE_X].value > max.value){
+            max = cached_block_result[i + coorForReFind * GRID_SIZE_X];
+        }
+//        printf("%d\n", cached_block_result[i + coorForReFind * GRID_SIZE_X]);
+    }
+    cache_for_vert_array[coorForReFind] = max;
+
+    CustomPoint maxI = cache_for_vert_array[0];
+    for(int i = 1; i != GRID_SIZE_Y; i++){
+        if(cache_for_vert_array[i].value > maxI.value){
+            maxI = cache_for_vert_array[i];
+        }
+    }
+    *result = maxI;
+    image[maxI.y * cols + maxI.x] = 0;
 }
 
 
@@ -202,6 +236,9 @@ int main()
 {
     cv::Mat ImageData = cv::imread("E:/Qt/Projects/MaxOnCuda/for_find_max.jpg");
     cv::circle(ImageData, cv::Point(500, 500), 0, cv::Scalar(255,255,255));
+    cv::circle(ImageData, cv::Point(501, 501), 0, cv::Scalar(254,254,254));
+    cv::circle(ImageData, cv::Point(499, 500), 0, cv::Scalar(254,254,254));
+    cv::circle(ImageData, cv::Point(209, 100), 0, cv::Scalar(252,252,252));
     cv::Mat GrayImage;
     cv::cvtColor(ImageData, GrayImage, cv::COLOR_BGR2GRAY);
     GrayImage = GrayImage(cv::Rect(0,0,1280,704));
@@ -214,65 +251,103 @@ int main()
     cudaEventRecord(start, 0);
 
     unsigned char *dev_Image;
+
     cudaMalloc((void**)&dev_Image, sizeof(unsigned char) * ImageData.cols * ImageData.rows);
+
     cudaMemcpy(dev_Image, (unsigned char *)GrayImage.ptr<unsigned char>(0), GrayImage.cols * GrayImage.rows * sizeof(unsigned char), cudaMemcpyHostToDevice);
 
     dim3 gridSize (ceil(ImageData.cols / (float)THREAD_DIM), ceil(ImageData.rows / (float)THREAD_DIM));
     dim3 blockSize (THREAD_DIM, THREAD_DIM);
 
-    GetMaxGrid<<<gridSize, blockSize>>>(
-                dev_Image,
-                ImageData.rows,
-                ImageData.cols
-            );
-    cudaDeviceSynchronize();
+    std::array<CustomPoint, NUMBER_OF_POINTS> p;
+    for(int i=0; i!= NUMBER_OF_POINTS; i++){
+        if(i == 0){
+            GetMaxGrid<<<gridSize, blockSize>>>(
+                        dev_Image,
+                        ImageData.rows,
+                        ImageData.cols,
+                        0,
+                        0
+                    );
+            cudaDeviceSynchronize();
 
-    int* dev_res_x;
-    int* dev_res_y;
-    int* dev_res_value;
-    cudaMalloc((void**)&dev_res_x, sizeof(int));
-    cudaMalloc((void**)&dev_res_y, sizeof(int));
-    cudaMalloc((void**)&dev_res_value, sizeof(int));
+            CustomPoint* dev_result;
+            cudaMalloc((void**)&dev_result, sizeof(CustomPoint));
 
-    FindVertMax<<<dim3(1,1), dim3(1,GRID_SIZE_Y)>>>(
-                  dev_res_x,
-                  dev_res_y,
-                  dev_res_value
-                  );
+            FindVertMaxAll<<<dim3(1,1), dim3(1,GRID_SIZE_Y)>>>(
+                          dev_Image,
+                          dev_result,
+                          ImageData.cols
+                          );
 
-    int* result_x = (int*)malloc(sizeof(int));
-    int* result_y = (int*)malloc(sizeof(int));
-    int* result_value = (int*)malloc(sizeof(int) * GRID_SIZE_Y);
+            CustomPoint* result = (CustomPoint*)malloc(sizeof(CustomPoint));
+            cudaMemcpy(result, dev_result, sizeof(CustomPoint), cudaMemcpyDeviceToHost);
 
-    cudaMemcpy(result_x, dev_res_x, sizeof(int), cudaMemcpyDeviceToHost);
-    cudaMemcpy(result_y, dev_res_y, sizeof(int), cudaMemcpyDeviceToHost);
-    cudaMemcpy(result_value, dev_res_value, sizeof(int), cudaMemcpyDeviceToHost);
+            p[i] = *result;
+
+            cudaFree(dev_result);
+            free(result);
+        }
+        else{
+            //1 найти координаты блока, к которому относится прошлый макс
+            //2 найти следующий максимум в этом блоке
+            //3 закрасить его, чтобы на следующей итерации не учавствовал(проблема)
+            //3 найти новый максимум в строке(но наверное лучше в столбце) глобальной сетки, обновить кэш
+            //4 найти новый максимум по новому столбцу
+
+            int blockIdx_x = p[i-1].x / THREAD_DIM;
+            int blockIdx_y = p[i-1].y / THREAD_DIM;
+
+            GetMaxGrid<<<gridSize, blockSize>>>(
+                        dev_Image,
+                        ImageData.rows,
+                        ImageData.cols,
+                        blockIdx_x,
+                        blockIdx_y
+                    );
+            cudaDeviceSynchronize();
+
+            CustomPoint* dev_result;
+            cudaMalloc((void**)&dev_result, sizeof(CustomPoint));
+
+            FindVertMaxOne<<<1, 1>>>(
+                          dev_Image,
+                          dev_result,
+                          blockIdx_y,
+                          ImageData.cols
+                          );
+
+            CustomPoint* result = (CustomPoint*)malloc(sizeof(CustomPoint));
+            cudaMemcpy(result, dev_result, sizeof(CustomPoint), cudaMemcpyDeviceToHost);
+
+            p[i] = *result;
+
+            cudaFree(dev_result);
+            free(result);
+        }
+    }
 
     cudaEventRecord(stop, 0);
     cudaEventSynchronize(stop);
 
-    std::cout << "result is: value = " << *result_value << " x = " << *result_x << "  y = " << *result_y << "\n";
+    for(int i=0; i!= NUMBER_OF_POINTS; i++){
+        std::cout << "result is: value = " << p[i].value << " x = " << p[i].x << "  y = " << p[i].y << "\n";
+
+        cv::Point max{p[i].x, p[i].y};
+        cv::circle(ImageData, max, 3, cv::Scalar(0,0,255));
+    }
+
+    cv::imshow("f", ImageData);
+    cv::waitKey(0);
 
     float elapsedTime;
     cudaEventElapsedTime(&elapsedTime, start, stop);
     printf("Time of CUDA Work: %3.1f ms\n", elapsedTime);
 
-    cv::Point max{*result_x, *result_y};
-    cv::circle(ImageData, max, 3, cv::Scalar(0,0,255));
-    cv::imshow("f", ImageData);
-    cv::waitKey(0);
-
     cudaEventDestroy( start );
     cudaEventDestroy( stop  );
 
-    free(result_x);
-    free(result_y);
-    free(result_value);
-
     cudaFree(dev_Image);
-    cudaFree(dev_res_x);
-    cudaFree(dev_res_y);
-    cudaFree(dev_res_value);
 }
 
 
